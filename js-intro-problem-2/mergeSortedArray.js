7df54c634ebb1562822a467ca331057c915ab32d
kaush <emailtokaush@gmail.com>
2020-04-20 15:19:26 -0400

    merge sorted two arrays excercise added

---
+++ js-intro-problem-2/mergeSortedArray.js
@@ -0,0 +1,25 @@
+//given sorted two arrays needs to merge into one single sorted array
+//mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]);
+// result should be => [ 0, 3, 3, 4, 4, 6, 30, 31 ]
+
+function mergeSortedArrays(arr1, arr2) {
+  const mergedArry = [];
+  let i = 0;
+  let j = 0;
+  let array1Item = arr1[i];
+  let array2Item = arr2[j];
+
+  while (array1Item || array2Item) {
+    if (array1Item === undefined || array1Item > array2Item) {
+      mergedArry.push(array2Item);
+      j++;
+      array2Item = arr2[j];
+    } else {
+      mergedArry.push(array1Item);
+      i++;
+      array1Item = arr1[i];
+    }
+  }
+  return mergedArry;
+}
+console.log(mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]));
