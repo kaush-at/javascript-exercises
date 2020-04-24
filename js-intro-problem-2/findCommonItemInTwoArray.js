505a3485749593a711ac82d354587c0887dba519
kaush <emailtokaush@gmail.com>
2020-04-24 14:24:11 -0400

    another solution added

--- js-intro-problem-2/findCommonItemInTwoArray.js
+++ js-intro-problem-2/findCommonItemInTwoArray.js
@@ -61,3 +61,9 @@ function containsCommonItem2(arr1, arr2) {
   }
   return false;
 }
+
+//----------- shortes way ----------------
+
+function containsCommonItem3(arr1, arr2) {
+  return arr1.some((item) => arr2.includes(item));
+}
