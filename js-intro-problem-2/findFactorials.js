097e1b251151ae7b89c0fc603798e6679e957f47
kaush <emailtokaush@gmail.com>
2020-04-26 16:56:07 -0400

    factorials function added using while loop

--- js-intro-problem-2/findFactorials.js
+++ js-intro-problem-2/findFactorials.js
@@ -1,5 +1,6 @@
 // factorial -> 5!=5*4*3*2*1
 
+//Iterative function using for loop
 function findFactorialIteratively1(number) {
   let answer = 1;
   if (number <= 0) {
@@ -11,4 +12,19 @@ function findFactorialIteratively1(number) {
   }
   return answer;
 }
+
+//Iterative function using while loop
+function findFactorialIteratively2(number) {
+  if (number <= 2) {
+    return number;
+  }
+  let answer = number;
+  while (number !== 1) {
+    answer = answer * (number - 1);
+    number--;
+  }
+  return answer;
+}
+
 console.log(findFactorialIteratively1(5));
+console.log(findFactorialIteratively2(5));
