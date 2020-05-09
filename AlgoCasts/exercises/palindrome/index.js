372330250948129e9c5ccd98907b5c1dd38c9b40
kaush <emailtokaush@gmail.com>
2020-05-09 10:07:04 -0400

    palindrome solution added

--- AlgoCasts/exercises/palindrome/index.js
+++ AlgoCasts/exercises/palindrome/index.js
@@ -7,6 +7,30 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // this is not the best solution say it to him and comeup with other solutions
+  return str.split("").every((char, i) => {
+    return char === str[str.length - 1 - i];
+  });
+}
 
 module.exports = palindrome;
+
+//method 1
+// function palindrome(str) {
+//   let revStr = "";
+//   for (char of str) {
+//     revStr = char + revStr;
+//   }
+//   if (revStr === str) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+//2nd method
+// function palindrome(str) {
+//   const reversed = str.split("").reverse().join("");
+//   return reversed === str;
+// }
